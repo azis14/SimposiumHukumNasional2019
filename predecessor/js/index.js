5089ba46053b7367a2621c0f5d486da918f82517
kevinasyraf <kevinasyrafputra@gmail.com>
2019-08-30 19:53:40 +0700

    added predecessor page, changed text for registration, delete video, added document for downloads

---
+++ predecessor/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $('body').addClass('loaded');
+  }, 2000);
+});
+
+// const body = document.querySelector('body');
+
+// window.addEventListener('DOMContentLoaded', e => {
+//   body.classList.add('loaded');
+// });
